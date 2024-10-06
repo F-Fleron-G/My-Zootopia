9f1760920828f1c2f419806aa32d5000ae52538d
Frederic G. Fleron <fleron.frederic@gmail.com>
2024-10-06 23:57:23 +0200

    improved the information cards html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,5 +1,6 @@
 import json
 
+
 def load_data(file_path):
     """Loads the JSON file. """
     with open(file_path, 'r') as handle:
@@ -7,22 +8,26 @@ def load_data(file_path):
 
 
 def create_animal_info(animals_data):
-    """ Creates a string with the animals' information,
-    and creates a new list for each animal.
+    """ Creates a string with the animals' information
+    listed in separate cards. One per animal.
     """
     animal_info = ""
     for animal in animals_data:
         animal_info += "<li class='cards__item'>\n"
 
         if "name" in animal:
-            animal_info += f"Name: {animal['name']}<br/>\n"
-        if "characteristics" in animal and "diet" in animal['characteristics']:
-            animal_info += f"Diet: {animal['characteristics']['diet']}<br/>\n"
-        if "locations" in animal and len(animal['locations']) > 0:
-            animal_info += f"Location: {animal['locations'][0]}<br/>\n"
-        if "characteristics" in animal and "type" in animal['characteristics']:
-            animal_info += f"Type: {animal['characteristics']['type']}<br/>\n"
+            animal_info += f"<div class='card__title'>{animal['name']}</div>\n"
 
+        animal_info += "<p class='card__text'>\n"
+
+        if "locations" in animal and len(animal["locations"]) > 0:
+            animal_info += f"<strong>Location:</strong> {animal["locations"][0]}<br/>\n"
+        if "characteristics" in animal and "type" in animal["characteristics"]:
+            animal_info += f"<strong>Type:</strong> {animal["characteristics"]["type"].capitalize()}<br/>\n"
+        if "characteristics" in animal and "diet" in animal["characteristics"]:
+            animal_info += f"<strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n"
+
+        animal_info += "</p>\n"
         animal_info += "</li>\n"
 
     print(animal_info)
@@ -30,16 +35,16 @@ def create_animal_info(animals_data):
 
 
 # Loads animal data from JSON.
-animals_data = load_data('animals_data.json')
+animals_data = load_data("animals_data.json")
 
 # 1. Read the content of the template.
 with open("animals_template.html", "r") as template_file:
     html_template = template_file.read()
 
-# 2. Generate a string with the animals’ data.
+# 2. Generate a string with the animals’ data in HTML.
 animal_info_html = create_animal_info(animals_data)
 
-# 3. Replace __REPLACE_ANIMALS_INFO__ with the generated string.
+# 3. Replace __REPLACE_ANIMALS_INFO__ with the generated HTML string.
 html_output = html_template.replace("__REPLACE_ANIMALS_INFO__", animal_info_html)
 
 # 4. Write the new HTML content to a new file.
