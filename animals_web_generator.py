dcf11c46dda46526ceec9a20234435a48163cc9a
Frederic G. Fleron <fleron.frederic@gmail.com>
2024-10-07 00:45:19 +0200

    improved PEP-8 standards

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -17,12 +17,15 @@ def serialize_animal(animal_obj):
     animal_info += "<p class='card__text'>\n"
 
     if "locations" in animal_obj and len(animal_obj["locations"]) > 0:
-        animal_info += f"<strong>Location:</strong> {animal_obj['locations'][0]}<br/>\n"
+        animal_info += f"<strong>Location:</strong> {animal_obj['locations']
+        [0]}<br/>\n"
     if "characteristics" in animal_obj:
         if "type" in animal_obj["characteristics"]:
-            animal_info += f"<strong>Type:</strong> {animal_obj['characteristics']['type'].capitalize()}<br/>\n"
+            animal_info += f"<strong>Type:</strong> {animal_obj['characteristics']
+            ['type'].capitalize()}<br/>\n"
         if "diet" in animal_obj["characteristics"]:
-            animal_info += f"<strong>Diet:</strong> {animal_obj['characteristics']['diet']}<br/>\n"
+            animal_info += f"<strong>Diet:</strong> {animal_obj['characteristics']
+            ['diet']}<br/>\n"
 
     animal_info += "</p>\n"
     animal_info += "</li>\n"
@@ -50,7 +53,8 @@ with open("animals_template.html", "r") as template_file:
 animal_info_html = create_animal_info(animals_data)
 
 # 3. Replace __REPLACE_ANIMALS_INFO__ with the generated HTML string.
-html_output = html_template.replace("__REPLACE_ANIMALS_INFO__", animal_info_html)
+html_output = html_template.replace("__REPLACE_ANIMALS_INFO__",
+                                    animal_info_html)
 
 # 4. Write the new HTML content to a new file.
 with open("animals.html", "w") as output_file:
