361db33fe3549546c3b97e3133495a758e71144e
Frederic G. Fleron <fleron.frederic@gmail.com>
2024-10-06 22:14:43 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,30 +1,43 @@
 import json
 
-
 def load_data(file_path):
-    """ Loads the JSON file """
+    """Loads the JSON file """
     with open(file_path, 'r') as handle:
         return json.load(handle)
 
 
-def print_animal_info(animals_data):
-    """ Prints each animal's Name, Diet, First Location, and Type (if available) """
+def create_animal_info(animals_data):
+    """ Creates a string with the animals' information """
+    animal_info = ""
     for animal in animals_data:
-        if 'name' in animal:
-            print(f"Name: {animal['name']}")
+        if "name" in animal:
+            animal_info += f"Name: {animal['name']}\n"
+        if "characteristics" in animal and "diet" in animal['characteristics']:
+            animal_info += f"Diet: {animal['characteristics']['diet']}\n"
+        if "locations" in animal and len(animal['locations']) > 0:
+            animal_info += f"Location: {animal['locations'][0]}\n"
+        if "characteristics" in animal and "type" in animal['characteristics']:
+            animal_info += f"Type: {animal['characteristics']['type']}\n"
+        animal_info += "\n"
 
-        if 'characteristics' in animal and 'diet' in animal['characteristics']:
-            print(f"Diet: {animal['characteristics']['diet']}")
-
-        if 'locations' in animal and len(animal['locations']) > 0:
-            print(f"Location: {animal['locations'][0]}")
-
-        if 'characteristics' in animal and 'type' in animal['characteristics']:
-            print(f"Type: {animal['characteristics']['type']}")
-
-        print()
+    print(animal_info)
+    return animal_info
 
 
+# Loads animal data from JSON
 animals_data = load_data('animals_data.json')
 
-print_animal_info(animals_data)
+# 1. Read the content of the template
+with open("animals_template.html", "r") as template_file:
+    html_template = template_file.read()
+
+# 2. Generate a string with the animals’ data
+animal_info_html = create_animal_info(animals_data)
+
+# 3. Replace __REPLACE_ANIMALS_INFO__ with the generated string
+html_output = html_template.replace("__REPLACE_ANIMALS_INFO__", animal_info_html)
+
+# 4. Write the new HTML content to a new file
+with open("animals.html", "w") as output_file:
+    output_file.write(html_output)
+
